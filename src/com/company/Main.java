9a8b6f8122b012a2dd5ad72785ef010a6fef27b7
Nahuel Venerus <nahuelvenerus@gmail.com>
2020-06-24 00:19:46 -0300

    First Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,21 +2,7 @@ package com.company;
 
 import java.util.Scanner;
 
-public class Main {static double inch = 2.54; // cms
-    static double foot = 12 * inch; // 30,48cms
-    static double yard = 3 * foot; // 91,44cms
-    static int meter = 100; // cms
-
-    private static double calculateInches(int givenNumber){
-        return (givenNumber* meter)/inch;
-    }
-    private static double calculateFeet(int givenNumber){
-        return (givenNumber* meter)/foot;
-    }
-    private static double calculateYards(int givenNumber){
-        return (givenNumber* meter)/yard;
-    }
-
+public class Main {
     public static void main(String[] args) {
         /*
         Leer una medida en metros e imprimir esta medida expresada en centímetros, pulgadas, pies y yardas. Los factores de conversión son:
@@ -25,15 +11,18 @@ public class Main {static double inch = 2.54; // cms
         pulgada = 2,54 cm.
         metro = 100 cm.nm
          */
-
-        int metrosRecibidos;
+        double inch = 2.54; // cms
+        double foot = 12 * inch; // 30,48cms
+        double yard = 3 * foot; // 91,44cms
+        int meter = 100; // cms
 
         System.out.println("Dame una distancia en metros y te dire cuanto es en centimetros, pulgadas, pies y yardas");
-        int givenNumber;
+        int givenMeters;
         Scanner reader = new Scanner(System.in);
-        givenNumber = reader.nextInt();
-        System.out.println("Pulgadas: " + calculateInches(givenNumber));
-        System.out.println("Pies: " + calculateFeet(givenNumber));
-        System.out.println("Yardas: " + calculateYards(givenNumber));
+        givenMeters = reader.nextInt();
+        System.out.println("Pulgadas: " + (givenMeters* meter)/inch);
+        System.out.println("Pies: " + (givenMeters* meter)/foot);
+        System.out.println("Yardas: " + (givenMeters* meter)/yard);
+        System.out.println("Centimetros: " + meter*100);
     }
 }
